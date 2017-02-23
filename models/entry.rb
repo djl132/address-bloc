a6c8045231c6cfe15a8b4f7c18deb707ca607f7d
DerekLin <djl132@case.edu>
2017-02-22 18:11:30 -0800

    Tested Entry Class

--- models/entry.rb
+++ models/entry.rb
@@ -1,3 +1,15 @@
 class Entry
-  
+  # you can define an attr access for attributes that do not exist
+  attr_accessor :name, :phone_number, :email
+
+  def initialize(name, phone_number, email)
+    @name = name
+    @phone_number = phone_number
+    @email = email
+  end
+
+  def to_s
+     "Name: #{name}\nPhone Number: #{phone_number}\nEmail: #{email}"
+   end
+
 end
