83f8a8c279ff6cb14e3d6f1d0239ae576d9fd720
DerekLin <djl132@case.edu>
2017-02-25 15:04:45 -0800

    fixed some errors

--- models/address_book.rb
+++ models/address_book.rb
@@ -4,7 +4,7 @@ require "csv"
 
 
 class AddressBook
-  attr_reader :entries
+  attr_reader :entries # creates an entry  for each AddressBook instance
   def initialize
     @entries = []
   end
@@ -34,6 +34,15 @@ class AddressBook
    end
 
    def import_from_csv(file_name)
+     csv_text = File.read(file_name) #reads file ???
+     #change to iterable table????? THINK SQL?
+     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+      csv.each do |row|
+       row_hash = row.to_hash #create an object with each object's header to its value 
+       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+     end
+
+      puts "#{entries.inspect}"
    end
 
 end
